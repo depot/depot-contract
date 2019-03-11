24780cd4aa356d12ffe1b68de0a8e10edb9ae4a0
Beau Simensen <beau@dflydev.com>
2019-03-11 11:31:02 -0500

    Modernize

--- tests/unit/SimplePhpFqcnContractResolverTest.php
+++ tests/unit/SimplePhpFqcnContractResolverTest.php
@@ -4,7 +4,7 @@ namespace Depot\Testing\Unit\Contract;
 
 use Depot\Contract\Contract;
 use Depot\Contract\SimplePhpFqcnContractResolver;
-use PHPUnit_Framework_TestCase as TestCase;
+use PHPUnit\Framework\TestCase;
 
 class SimplePhpFqcnContractResolverTest extends TestCase
 {
